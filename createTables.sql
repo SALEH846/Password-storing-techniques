9d86612af97807df2f83ace94841b43d40886451
Muhammad Saleh <ranaasaleh846@gmail.com>
2023-07-09 19:26:39 +0500

    Script to create SQL tables for storing passwords and other user data

---
+++ createTables.sql
@@ -0,0 +1,25 @@
+create table auth1 (
+	username text,
+	password text
+);
+
+create table auth2 (
+	username text,
+	password text
+);
+
+create table auth3 (
+	username text,
+	password text,
+	salt text
+);
+
+create table auth4 (
+	username text,
+	password text
+);
+
+create table auth5 (
+	username text,
+	userdata text
+);
